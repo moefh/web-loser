502c8c5fa4773c3516be0d5f67453625abdb5d41
guto.motta@gmail.com <guto.motta@gmail.com@ef42d7ff-c041-aff2-0911-896348e6819a>
2011-07-03 23:54:21 +0000

    Simplified keyboard handler

--- trunk/code/keyboard.js
+++ trunk/code/keyboard.js
@@ -8,14 +8,25 @@ var KEY_SHOOT = 88;
 function Keyboard() {
     this.keys_down = [];
     this.keys_pressed = [];
+//    this.noRepeat = {KEY_SHOOT};
     
     var self = this;
     document.onkeydown = function(ev) { self.ev_keydown(ev); };
     document.onkeyup = function(ev) { self.ev_keyup(ev); };
 }
 
+Keyboard.prototype.keyPressed = function(key){
+    return(this.keys_pressed.indexOf(key) >= 0);
+};
+
+Keyboard.prototype.keyDown = function(key){
+    return(this.keys_down.indexOf(key) >= 0);
+}
+
 Keyboard.prototype.ev_keydown = function(e) {
+/*
     var e = window.event || e;
+
     if (e.keyCode > 256) return;
     
     while (this.keys_down.length < e.keyCode) this.keys_down[this.keys_down.length] = false;
@@ -24,18 +35,34 @@ Keyboard.prototype.ev_keydown = function(e) {
     //console.log("(" + key_rep + ") key pressed: " + e.keyCode, true);
     while (this.keys_pressed.length < e.keyCode) this.keys_pressed[this.keys_pressed.length] = false;
     this.keys_pressed[e.keyCode] = true;
+*/
+    if(!this.keyDown(e.keyCode)){
+        this.keys_down.push(e.keyCode);
+    }
+    if(!this.keyPressed(e.keyCode)){
+        this.keys_pressed.push(e.keyCode);
+    }
 };
 
 Keyboard.prototype.ev_keyup = function(e) {
+/*
     var e = window.event || e;
+
     if (e.keyCode > 256) return;
     
     while (this.keys_down.length < e.keyCode) this.keys_down[this.keys_down.length] = false;
     this.keys_down[e.keyCode] = false;
+*/
+    idx = this.keys_down.indexOf(e.keyCode);
+    if(idx >= 0)
+        this.keys_down[idx] = 0;
 };
 
 Keyboard.prototype.update = function() {
+/*
     for (var i = 0; i < this.keys_pressed.length; i++)
         if (this.keys_pressed[i])
             this.keys_pressed[i] = false;
+*/
+    this.keys_pressed.length = 0;
 };
