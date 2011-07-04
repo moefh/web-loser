3e4a20d3d62ed42ee4a7015bdbea2fec1b5a225e
guto.motta@gmail.com <guto.motta@gmail.com@ef42d7ff-c041-aff2-0911-896348e6819a>
2011-07-04 00:48:08 +0000

    Improved keyboard handler. Fixed arrays management. Fixed keyPressed - no more fireball storm :(

--- trunk/code/keyboard.js
+++ trunk/code/keyboard.js
@@ -8,61 +8,57 @@ var KEY_SHOOT = 88;
 function Keyboard() {
     this.keys_down = [];
     this.keys_pressed = [];
-//    this.noRepeat = {KEY_SHOOT};
+    this.keys_wait = [];
     
     var self = this;
     document.onkeydown = function(ev) { self.ev_keydown(ev); };
     document.onkeyup = function(ev) { self.ev_keyup(ev); };
 }
-
+/*
+ * Search for KEY in the different keymaps
+ */
 Keyboard.prototype.keyPressed = function(key){
     return(this.keys_pressed.indexOf(key) >= 0);
 };
-
 Keyboard.prototype.keyDown = function(key){
     return(this.keys_down.indexOf(key) >= 0);
 }
-
-Keyboard.prototype.ev_keydown = function(e) {
+Keyboard.prototype.keyWait = function(key){
+    return(this.keys_wait.indexOf(key) >= 0);
+}
 /*
-    var e = window.event || e;
-
-    if (e.keyCode > 256) return;
-    
-    while (this.keys_down.length < e.keyCode) this.keys_down[this.keys_down.length] = false;
-    this.keys_down[e.keyCode] = true;
-
-    //console.log("(" + key_rep + ") key pressed: " + e.keyCode, true);
-    while (this.keys_pressed.length < e.keyCode) this.keys_pressed[this.keys_pressed.length] = false;
-    this.keys_pressed[e.keyCode] = true;
-*/
+ * Removes element E from array
+ * Array method, maybe place it somewhere else?
+ */
+Array.prototype.remove = function(e){
+    var idx = this.indexOf(e);
+    if(idx >= 0){
+        this[idx] = this[this.length - 1];
+        this.length--;
+    }
+}
+/*
+ * Keydown handler
+ */
+Keyboard.prototype.ev_keydown = function(e) {
     if(!this.keyDown(e.keyCode)){
         this.keys_down.push(e.keyCode);
     }
-    if(!this.keyPressed(e.keyCode)){
+    if(!this.keyPressed(e.keyCode) && !this.keyWait(e.keyCode)){
         this.keys_pressed.push(e.keyCode);
+        this.keys_wait.push(e.keyCode);
     }
 };
-
+/*
+ * Keyup handler
+ */
 Keyboard.prototype.ev_keyup = function(e) {
-/*
-    var e = window.event || e;
-
-    if (e.keyCode > 256) return;
-    
-    while (this.keys_down.length < e.keyCode) this.keys_down[this.keys_down.length] = false;
-    this.keys_down[e.keyCode] = false;
-*/
-    idx = this.keys_down.indexOf(e.keyCode);
-    if(idx >= 0)
-        this.keys_down[idx] = 0;
+    this.keys_down.remove(e.keyCode);
+    this.keys_wait.remove(e.keyCode);
 };
-
-Keyboard.prototype.update = function() {
 /*
-    for (var i = 0; i < this.keys_pressed.length; i++)
-        if (this.keys_pressed[i])
-            this.keys_pressed[i] = false;
-*/
+ * Called by step, clears keys_pressed, keeps keys_down
+ */
+Keyboard.prototype.update = function() {
     this.keys_pressed.length = 0;
 };
