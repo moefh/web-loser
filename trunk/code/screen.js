0eeb02343a83b15325e99eb1319883c562298a31
ricardo.massaro <ricardo.massaro@ef42d7ff-c041-aff2-0911-896348e6819a>
2011-07-05 01:05:04 +0000

    Remove scaling code from screen class (scaling was always 1:1 anyways).

--- trunk/code/screen.js
+++ trunk/code/screen.js
@@ -1,34 +1,21 @@
 
 function Screen(lw, lh, canvas)
 {
-    this.canvas = canvas;
-    this.ctx = canvas.getContext('2d');
-    
     this.x = 0;
     this.y = 0;
-    
-    this.logical_w = Math.round(lw);
-    this.logical_h = Math.round(lh);
+    this.w = c_int(lw);
+    this.h = c_int(lh);
 
-    canvas.width = this.logical_w;
-    canvas.height = this.logical_h;
-
-    this.w = canvas.width;
-    this.h = canvas.height;
-
-    this.scale_x = this.w / this.logical_w;
-    this.scale_y = this.h / this.logical_h;
-    
-    this.tile_w = 64 * this.scale_x;
-    this.tile_h = 64 * this.scale_y;
-
-    this.message = "";
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');    
+    this.canvas.width = this.w;
+    this.canvas.height = this.h;
 }
 
 Screen.prototype.getSize = function(){
     return {
-        'width': this.logical_w,
-        'height': this.logical_h
+        'width': this.w,
+        'height': this.h
     };
 };
 
@@ -48,28 +35,28 @@ Screen.prototype.draw_map_bg = function(map) {
     // TODO: large viewport screws this
     if (map.get_bg_image() != null) {
         var img = map.get_bg_image();
-        if (img.width < this.logical_w || img.height < this.logical_h) {
-            this.ctx.drawImage(img, 0, 0, this.logical_w, this.logical_h);
+        if (img.width < this.w || img.height < this.h) {
+            this.ctx.drawImage(img, 0, 0, this.w, this.h);
         }
         else {
-            var x = c_int(this.x * (img.width - this.logical_w) / (map.w * 64 - this.logical_w));
-            var y = c_int(this.y * (img.height - this.logical_h) / (map.h * 64 - this.logical_h));
+            var x = c_int(this.x * (img.width - this.w) / (map.w * 64 - this.w));
+            var y = c_int(this.y * (img.height - this.h) / (map.h * 64 - this.h));
             this.ctx.drawImage(img, -x, -y);
         }
     }
     
     var tile_start_x = c_int(this.x / 64);
     var tile_start_y = c_int(this.y / 64);
-    for (var y = 0; y < (this.logical_h/64)+1; y++) {
-        for (var x = 0; x < (this.logical_w/64)+1; x++) {
-            var pos_x = (64*x - this.x%64) * this.scale_x;
-            var pos_y = (64*y - this.y%64) * this.scale_y;
+    for (var y = 0; y < (this.h/64)+1; y++) {
+        for (var x = 0; x < (this.w/64)+1; x++) {
+            var pos_x = 64*x - this.x%64;
+            var pos_y = 64*y - this.y%64;
             
             var tile_id = map.bg_tile(tile_start_x + x, tile_start_y + y);
             if (tile_id != 0xffff) {
                 var tile_x = tile_id % 16;
                 var tile_y = c_int(tile_id / 16);
-                this.ctx.drawImage(map.get_image(), tile_x*64, tile_y*64, 64, 64, pos_x, pos_y, this.tile_w, this.tile_h);
+                this.ctx.drawImage(map.get_image(), tile_x*64, tile_y*64, 64, 64, pos_x, pos_y, 64, 64);
             }
         }
     }
@@ -79,16 +66,16 @@ Screen.prototype.draw_map_fg = function(map) {
     //debug("draw_map: screen_x=" + screen_x + ", screen_y=" + screen_y);
     var tile_start_x = c_int(this.x / 64);
     var tile_start_y = c_int(this.y / 64);
-    for (var y = 0; y < (this.h/this.tile_h)+1; y++) {
-        for (var x = 0; x < (this.w/this.tile_w)+1; x++) {
-            var pos_x = (64*x - this.x%64) * this.scale_x;
-            var pos_y = (64*y - this.y%64) * this.scale_y;
+    for (var y = 0; y < (this.h/64)+1; y++) {
+        for (var x = 0; x < (this.w/64)+1; x++) {
+            var pos_x = 64*x - this.x%64;
+            var pos_y = 64*y - this.y%64;
 
             var tile_id = map.fg_tile(tile_start_x + x, tile_start_y + y);
             if (tile_id != 0xffff) {
                 var tile_x = tile_id % 16;
                 var tile_y = c_int(tile_id / 16);
-                this.ctx.drawImage(map.get_image(), tile_x*64, tile_y*64, 64, 64, pos_x, pos_y, this.tile_w, this.tile_h);
+                this.ctx.drawImage(map.get_image(), tile_x*64, tile_y*64, 64, 64, pos_x, pos_y, 64, 64);
             }
         }
     }
@@ -98,8 +85,8 @@ Screen.prototype.draw_frame = function(image, index, frame_w, frame_h, x, y) {
     var img_x = index % 16;
     var img_y = c_int(index / 16);
     this.ctx.drawImage(image, img_x*frame_w, img_y*frame_h, frame_w, frame_h,
-                       x*this.scale_x, y*this.scale_y,
-                       frame_w*this.scale_x, frame_h*this.scale_y);
+                       x, y,
+                       frame_w, frame_h);
 };
 
 Screen.prototype.draw_npc = function(images, npc) {
@@ -121,26 +108,26 @@ Screen.prototype.draw_npcs = function(images, npcs, follow_npc) {
 };
 
 Screen.prototype.draw = function(images, map, npcs, follow_npc) {
-    var border_w = c_int(0.35*this.logical_w);
-    var border_h = c_int(0.35*this.logical_h);
-
     // change the screen position to follow a NPC, if necessary
     if (follow_npc != null) {
+        var border_w = c_int(0.35*this.w);
+        var border_h = c_int(0.35*this.h);
+
         if (this.x > follow_npc.x - border_w) this.x = follow_npc.x - border_w;
         if (this.y > follow_npc.y - border_h) this.y = follow_npc.y - border_h;
-        if (this.x + this.logical_w < follow_npc.x + follow_npc.def.w + border_w)
-            this.x = follow_npc.x + follow_npc.def.w + border_w - this.logical_w;
-        if (this.y + this.logical_h < follow_npc.y + follow_npc.def.h + border_h)
-            this.y = follow_npc.y + follow_npc.def.h + border_h - this.logical_h;
+        if (this.x + this.w < follow_npc.x + follow_npc.def.w + border_w)
+            this.x = follow_npc.x + follow_npc.def.w + border_w - this.w;
+        if (this.y + this.h < follow_npc.y + follow_npc.def.h + border_h)
+            this.y = follow_npc.y + follow_npc.def.h + border_h - this.h;
     }
 
     // limit the the screen position to be inside the map
     if (this.x < 0) this.x = 0;
     if (this.y < 0) this.y = 0;
-    if (this.x > map.w * 64 - this.logical_w)
-        this.x = map.w * 64 - this.logical_w;
-    if (this.y > map.h * 64 - this.logical_h)
-        this.y = map.h * 64 - this.logical_h;
+    if (this.x > map.w * 64 - this.w)
+        this.x = map.w * 64 - this.w;
+    if (this.y > map.h * 64 - this.h)
+        this.y = map.h * 64 - this.h;
     
     // draw everything
     this.draw_map_bg(map);
