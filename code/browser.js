9044cb66ea30f09f856409520a0df2097c824774
guto.motta <guto.motta@gmail.com>
2011-07-05 01:31:36 +0000

    Improved animation for OSD button

--- code/browser.js
+++ code/browser.js
@@ -25,11 +25,39 @@ function Browser(screen, event) {
  */
 Browser.prototype.osd = function(msg){
     $('#messages').hide();
-    $('#messages').empty().append('<span>'+msg+'</span>');
+    $('#messages').empty().append('<span id="msg">'+msg+'</span>');
+
     $('#messages').fadeIn('fast');
+    // #messages need to be 'visible' to get jQuery's width and height
+    var padding = 20;
+    var marginLeft = -Math.round($('#msg').width()/2 + padding);
+    var marginTop = -Math.round($('#msg').height()/2 + padding);
+    
+    $('#msg').css({
+        'marginLeft' : marginLeft,
+        'marginTop' : marginTop,
+        'padding-left' : padding,
+        'padding-right' : padding,
+        'padding-top' : padding,
+        'padding-bottom' : padding,
+    });
 };
 Browser.prototype.osdClear = function(){
-    $('#messages').fadeOut('fast');
+    var padding = 200;
+    var marginLeft = -Math.round($('#msg').width()/2 + padding);
+    var marginTop = -Math.round($('#msg').height()/2 + padding);
+
+    $('#msg').animate({
+        'marginLeft': marginLeft,
+        'marginTop': marginTop,
+        'padding-left' : padding,
+        'padding-right' : padding,
+        'padding-top' : padding,
+        'padding-bottom' : padding,
+        'opacity' : 0
+    }, 'fast', function(){
+        $('#messages').empty();
+    });
 };
 /**
  * Handles .centerBox class, changing size of main box objects
